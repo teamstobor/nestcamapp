d584389d51629c0c04c025d91cf2291b6f54d9c0
Sambasiva Andaluri <samrao@andaluri.com>
2018-02-25 09:18:08 -0500

    Fix host and port

--- stream.py
+++ stream.py
@@ -81,7 +81,7 @@ with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
     output = StreamingOutput()
     camera.start_recording(output, format='mjpeg')
     try:
-        address = ('', 80)
+        address = ('0.0.0.0', 80)
         server = StreamingServer(address, StreamingHandler)
         server.serve_forever()
     finally:
