93d1eac542f7fc490d507c67a91387e86f1b4e71
Sambasiva Andaluri <samrao@andaluri.com>
2018-05-30 18:42:48 -0400

    Remove network manager

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+
+# Start resin-wifi-connect
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+
 modprobe i2c-dev
 modprobe v4l2_common
 
 # Run one process loop
 python process.py
 
-# Start resin-wifi-connect
-export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 ./wifi-connect
 
 python -m SimpleHTTPServer 80 stream.py &
