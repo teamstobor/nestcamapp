68f31789210520b7f677e93d4fa4c5594488d28c
Sambasiva Andaluri <samrao@andaluri.com>
2018-06-17 14:40:20 -0400

    new timelapse

--- start.sh
+++ start.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-# Start resin-wifi-connect
-export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-
 modprobe i2c-dev
 modprobe v4l2_common
 
-# Run one process loop
-python process.py
-
-./wifi-connect
-
-python -m SimpleHTTPServer 80 stream.py &
-
+sudo systemctl start timelapse
