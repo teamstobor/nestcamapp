932b218a31b622e8b12b517833c21735fa98567d
Sambasiva Andaluri <samrao@andaluri.com>
2018-06-23 23:37:03 -0400

    fix service

--- startWebServer.sh
+++ startWebServer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 systemctl daemon-reload
-systemctl enable capture-image
-systemctl start capture-image
-systemctl enable process-images
-systemctl start process-images
+systemctl enable capture-image.timer
+systemctl start capture-image.timer
+systemctl enable process-images.timer
+systemctl start process-images.timer
 python -m SimpleHTTPServer 80 > /dev/null 2>&1 &
 journalctl -f
